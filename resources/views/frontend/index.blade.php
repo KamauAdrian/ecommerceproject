7ea17a8e57c372031c5b72f14b09f040e3869e6d
KamauAdrian <35832303+KamauAdrian@users.noreply.github.com>
2019-05-17 14:22:20 +0300

    another commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,22 +14,16 @@
                         <h2 class="title text-center">Features Items</h2>
                         @foreach($products as $product)
                         <div class="col-sm-4">
-                        <div class="product-image-wrapper">
-                        <div class="single-products">
-                        <div class="productinfo text-center">
-                        <a href="{{url('/product-detail',$product->id)}}"><img src="{{url('products/small/',$product->image)}}" alt="" /></a>
-                        <h2>$ {{$product->price}}</h2>
-                        <p>{{$product->p_name}}</p>
-                        <a href="{{url('/product-detail',$product->id)}}" class="btn btn-default add-to-cart">View Product</a>
-                        </div>
-                        </div>
-                        <div class="choose">
-                        <ul class="nav nav-pills nav-justified">
-                        <li><a href="#"><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
-                        <li><a href="#"><i class="fa fa-plus-square"></i>Add to compare</a></li>
-                        </ul>
-                        </div>
-                        </div>
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="{{url('/product-detail',$product->id)}}"><img src="{{url('products/small/',$product->image)}}" alt="" /></a>
+                                        <h2>$ {{$product->price}}</h2>
+                                        <p>{{$product->p_name}}</p>
+                                        <a href="{{url('/product-detail',$product->id)}}" class="btn btn-default add-to-cart">View Product</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         @endforeach
                     </div>
